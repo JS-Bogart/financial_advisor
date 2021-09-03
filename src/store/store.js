33f79d7f0c64c9818a83c2c73611a208fc66bb8f
Bogey <jeffreysbogart@gmail.com>
2021-09-03 13:43:17 -0700

    Add tableData

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
+import { preloadedState } from './tableData';
 
 const configureStore = (preloadedState = {}) => (
   createStore(
