ba61df851e89739d9870727ff6616d91cf952846
Bogey <jeffreysbogart@gmail.com>
2021-09-03 14:33:34 -0700

    Create table file

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Table from "./table";
 
 const Home = (props) => {
 
+  const [selectRisk, setSelectedRisk] = useState(null);
+  const [dataDisplay, setDataDisplay] = useState("table");
+
+  const getTable = () => {
+    return(
+      <Table />
+    )
+  }
+
   return(
     <div>
-      
+      <header>
+        <h1>Financial Advisor</h1>
+      </header>
+      <body>
+        
+      </body>
     </div>
   )
 }
---
+++ src/components/home/table.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const Table = (props) => {
+  return(
+    <div>
+
+    </div>
+  )
+}
+
+export default Table;
